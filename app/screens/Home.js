ad017ae67e3f42325b5e6dbb73cfc60fdf2a680b
vedant prasad priya karle <rays57529@gmail.com>
2022-02-21 23:12:07 +0530

    added home page tab navigation

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,13 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import React, { useContext } from "react";
+import { Text } from "react-native";
+import Context from "../context/Context";
+import Chats from "./Chats";
+import Photo from "./Photo";
 
 const Home = () => {
+	const Tab = createMaterialTopTabNavigator();
+
+	const {
+		theme: { colors },
+	} = useContext(Context);
+
 	return (
-		<View>
-			<StatusBar style='auto' />
-			<Text>Home</Text>
-		</View>
+		<Tab.Navigator
+			screenOptions={({ route }) => {
+				return {
+					tabBarLabel: () => {
+						if (route.name === "photo") {
+							return <Ionicons name='camera' size={20} color={colors.white} />;
+						} else {
+							return (
+								<Text style={{ color: colors.white }}>
+									{route.name.toLocaleUpperCase()}
+								</Text>
+							);
+						}
+					},
+					tabBarStyle: true,
+					tabBarLabelStyle: {
+						color: colors.white,
+					},
+					tabBarIndicatorStyle: {
+						backgroundColor: colors.white,
+					},
+					tabBarStyle: {
+						backgroundColor: colors.foreground,
+					},
+				};
+			}}
+			initialRouteName='chats'>
+			<Tab.Screen name='photo' component={Photo} />
+			<Tab.Screen name='chats' component={Chats} />
+		</Tab.Navigator>
 	);
 };
 
---
+++ app/screens/Photo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Photo = () => {
+	return (
+		<View>
+			<Text>Photo</Text>
+		</View>
+	);
+};
+
+export default Photo;
